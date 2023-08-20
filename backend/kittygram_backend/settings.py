e6d9af42b1d3593bbb0d3ade0ccb9c5b50dd31cb
Gennady Umikashvili <gennadyumikashvili@yandex.ru>
2023-08-20 17:21:41 +0300

    vol.2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ('158.160.30.10', '127.0.0.1', '0.0.0.0', 'localhost', 'kittygramyandexgu.ddns.net')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
